eddae1d401a43da9b0a950196fb0fb0047176772
HarshAryan06 <aryan.harsh06@gmail.com>
2026-08-13 00:20:49 +0530

    Json

--- week1/day4/json_pydantic.py
+++ week1/day4/json_pydantic.py
@@ -13,7 +13,27 @@ client=Groq(api_key= my_api_key )
 
 model="llama-3.3-70b-versatile"
 role="user"
-text = "Hello mu name is harsh i bought a iphone from your store .It stop wroking please contanct me on abc@gmail.com. My contact is 45989"
+
+# Structure format 
+from pydantic import BaseModel
+class Ticket(BaseModel):
+    name : str
+    email : str 
+    issue : str 
+schema = Ticket.model_json_schema()
+response_format = {
+    "type" : "json_object"
+}
+system_prompt = f"""
+Extract the personal information from the ticket stricly on this schema and give json output{schema}"""
+
+message_system = {
+    "role" : "system",
+    "content" : system_prompt
+}
+
+
+text = "Hello my name is harsh i bought a iphone from your store .It stop wroking please contanct me on abc@gmail.com. My contact is 45989"
 prompt=f""" 
         This is customer details please extract the personal information from this {text} """
 # message me role and content
@@ -22,11 +42,22 @@ message={
     "content": prompt
 }
 
-messages=[message]
+messages=[message_system,message]
 
 
-response=client.chat.completions.create(model=model, messages=messages)
+response=client.chat.completions.create(model=model, messages=messages, response_format = response_format)
 
 
 answer = response.choices[0].message.content
 print(answer)
+
+# isko padhta kasa hai 
+import json 
+raw_json = answer
+data_file = json.loads(raw_json)
+ticket = Ticket(**data_file)
+
+
+print(ticket.name)
+print(ticket.email)
+print(ticket.issue)
